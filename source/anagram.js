27bf7515d833a49c72a46739b4f206cbf422df9c
Ivan Gorshkov <ivan.a.gorshkov@gmail.com>
2021-02-08 21:53:24 +0300

    sdf

--- source/anagram.js
+++ source/anagram.js
@@ -1,13 +1,12 @@
-"use strict"
+'use strict';
 
-const anagram = (input) => {
-    const arr = input.filter((value,index) => !input.includes(value,index+1));
-    const sortedWords = arr.map((item) => item
-        .split('')
-        .sort()
-        .join('')
-    );
-
+/***
+ * get object of item:index value
+ *
+ * @param {array} sortedWords - array of sorted chars in words in alphabet order
+ * @returns {object}
+ */
+const getAnagramWordsIndex = (sortedWords) => {
     const indexMap = {};
     sortedWords.forEach(value => {
         indexMap[value] = sortedWords.reduce((accumulator, currentValue, index) => {
@@ -15,7 +14,17 @@ const anagram = (input) => {
             return accumulator;
         }, []);
     });
+    return indexMap;
+}
 
+/***
+ * get array of arrays of anagram words from indexMap and defualt array of words
+ *
+ * @param {object} indexMap - object of item:index value
+ * @param {array} arr - unique array of words
+ * @returns {array} - anagram array of arrays
+ */
+const getAnagramArray = (indexMap, arr) => {
     const anagramArray = [];
     for (const item in indexMap) {
         anagramArray.push(indexMap[item]
@@ -26,4 +35,25 @@ const anagram = (input) => {
     return anagramArray.filter((value) => {
         return value.length > 1;
     }).sort();
+}
+
+/***
+ * get array of arrays of anagram words input array of words
+ *
+ * @param {array} input - array of words
+ * @returns {array} - anagram array of arrays
+ */
+const anagram = (input) => {
+    const arr = input.filter((value,index) => !input.includes(value,index+1));
+    const sortedWords = arr.map((item) => item
+        .split('')
+        .sort()
+        .join('')
+    );
+
+    const indexMap = getAnagramWordsIndex(sortedWords);
+    const anagramArray = getAnagramArray(indexMap, arr);
+    return anagramArray;
 };
+
+
